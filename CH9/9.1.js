accc3868f640b532d9e58d2e5dc653b9694fd5c8
Pajan Panahian-Larki <pajan.pl@gmail.com>
2019-01-24 14:10:24 -0500

    Ch.9 Ex.1

--- CH9/9.1.js
+++ CH9/9.1.js
@@ -39,3 +39,47 @@ function verify(regexp, yes, no) {
     console.log(`Unexpected match for '${str}'`);
   }
 }
+
+// Second Try
+
+// Fill in the regular expressions
+
+verify(/\w*\sca\w*/,
+       ["my car", "bad cats"],
+       ["camper", "high art"]);
+
+verify(/\w*\s\w*/,
+       ["pop culture", "mad props"],
+       ["plop", "prrrop"]);
+
+verify(/ferr[^u]\w*/,
+       ["ferret", "ferry", "ferrari"],
+       ["ferrum", "transfer A"]);
+
+verify(/[ai]cious/,
+       ["how delicious", "spacious room"],
+       ["ruinous", "consciousness"]);
+
+verify(/\./,
+       ["bad punctuation ."],
+       ["escape the period"]);
+
+verify(/\w{7,}/,
+       ["hottentottententen"],
+       ["no", "hotten totten tenten"]);
+
+verify(/\w*\s\w{4,}/,
+       ["red platypus", "wobbling nest"],
+       ["earth bed", "learning ape", "BEET"]);
+
+
+function verify(regexp, yes, no) {
+  // Ignore unfinished exercises
+  if (regexp.source == "...") return;
+  for (let str of yes) if (!regexp.test(str)) {
+    console.log(`Failure to match '${str}'`);
+  }
+  for (let str of no) if (regexp.test(str)) {
+    console.log(`Unexpected match for '${str}'`);
+  }
+}
