3a5e3b3ae1a77c93cc4d002c6f0f6d4b566a9603
Pajan Panahian-Larki <ppl18@my.yorku.ca>
2019-07-28 16:34:23 -0400

    Started Ch.19 review

---
+++ CH19/Ch19Review.js
@@ -0,0 +1,24 @@
+        // Chapter 19 - Pixel Art Editor
+
+// This application will be a pixel drawing program, where you can modify a picture 
+// pixel by pixel by manipulating a zoomed-in view of it, shown as a grid of 
+// coloured squares. You can use the program to open image files, scribble on them 
+// with your mouse, and save them.  
+
+        // Components
+
+        // The State
+
+        // Dom Building
+
+        // The Canvas
+
+        // The Application
+
+        // Drawing Tools
+
+        // Saving and loading
+
+        // Undo history
+
+        // Let's Draw
