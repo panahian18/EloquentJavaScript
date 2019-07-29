be6bd2e3f1aaa7fc64eab6e04dd12d8c4f2ddb18
Pajan Panahian-Larki <ppl18@my.yorku.ca>
2019-07-29 02:27:42 -0400

    reviewed drawing tools section

--- CH19/Ch19Review.js
+++ CH19/Ch19Review.js
@@ -332,7 +332,86 @@ class ColorSelect {
 
         // Drawing Tools
 
+// Before we can draw anything, we need to implement the tools that will control 
+// the functionality of mouse or touch events on the canvas.
 
+// The most basic tools is the draw tool, which changes any pixel you click or tap
+// to the currently selected colour. It dispatches an action that updates the picture
+// to a version in which the pointed-at pixel is given the currently selected colour.
+
+function draw(pos, state, dispatch) {
+    function drawPixel({ x, y }, state) {
+        let drawn = { x, y, color: state.color };
+        dispatch({ picture: state.picture.draw([drawn]) });
+    }
+    drawPixel(pos, state);
+    return drawPixel;
+}
+
+// The function immediately calls the drawPixel function but then also returns it 
+// so that it is called again for newly touched pixels when the user drags or 
+// swipes over the picture. 
+
+// We also define a rectangle funciton, which is used to quickly draw rectangles. It
+// starts drawing from where you start dragging from to where you release the click. 
+
+function rectangle(start, state, dispatch) {
+    function drawRectangle(pos) {
+        let xStart = Math.min(start.x, pos.x);
+        let yStart = Math.min(start.y, pos.y);
+        let xEnd = Math.max(start.x, pos.x);
+        let yEnd = Math.max(start.y, pos.y);
+        let drawn = [];
+        for (let y = yStart; y <= yEnd; y++) {
+            for (let x = xStart; x <= xEnd; x++) {
+                drawn.push({ x, y, color: state.color });
+            }
+        }
+        dispatch({ picture: state.picture.draw(drawn) });
+    }
+    drawRectangle(start);
+    return drawRectangle;
+}
+
+// An important detail in this implementation is that when dragging, the rectangle
+// angle is redrawn on the picture from the original state. That way, you can make 
+// the rectangle larger and smaller again while creating it, without the 
+// intermediate rectangles sticking around in the final picture.
+
+// This is a tool that fills the pixel under the pointer and all adjacent pixels
+// that have the same colour. Adjacent meaning horizontally or vertically adjacent
+// not diagonally. 
+
+// This code is similar to the pathfinding code in chapter 7, however in this case 
+// it seraches for all connected pixels through a grid instead of just a route.
+
+const around = [{ dx: -1, dy: 0 }, { dx: 1, dy: 0 },
+{ dx: 0, dy: -1 }, { dx: 0, dy: 1 }];
+
+function fill({ x, y }, state, dispatch) {
+    let targetColor = state.picture.pixel(x, y);
+    let drawn = [{ x, y, color: state.color }];
+    for (let done = 0; done < drawn.length; done++) {
+        for (let { dx, dy } of around) {
+            let x = drawn[done].x + dx, y = drawn[done].y + dy;
+            if (x >= 0 && x < state.picture.width &&
+                y >= 0 && y < state.picture.height &&
+                state.picture.pixel(x, y) == targetColor &&
+                !drawn.some(p => p.x == x && p.y == y)) {
+                drawn.push({ x, y, color: state.color });
+            }
+        }
+    }
+    dispatch({ picture: state.picture.draw(drawn) });
+}
+
+// Pick is the final tool, this allows you to point at a colour in the picture and
+// use it as the current drawing colour.
+
+function pick(pos, state, dispatch) {
+    dispatch({ color: state.picture.pixel(pos.x, pos.y) });
+}
+                        
 
         // Saving and loading
 
@@ -340,7 +419,34 @@ class ColorSelect {
 
         // Let's Draw
 
+// 
 
+const startState = {
+    tool: "draw",
+    color: "#000000",
+    picture: Picture.empty(60, 30, "#f0f0f0"),
+    done: [],
+    doneAt: 0
+};
+const baseTools = { draw, fill, rectangle, pick };
+const baseControls = [
+    ToolSelect, ColorSelect, SaveButton, LoadButton, UndoButton
+];
+function startPixelEditor({ state = startState,
+    tools = baseTools,
+    controls = baseControls }) {
+    let app = new PixelEditor(state, {
+        tools,
+        controls,
+        dispatch(action) {
+            state = historyUpdateState(state, action);
+            app.syncState(state);
+        }
+    });
+    return app.dom;
+}
+        
+// 
 
 // 4:30-8:30 review
 // 8:30-9:30 React version
